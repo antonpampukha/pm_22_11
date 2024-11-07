ccfb38c369bb645c63ddb135dc68f98e2f3c4fbc
Anton Pampukha <antonpampuha4@gmail.com>
2024-11-08 00:20:09 +0200

    update data.json. Add some code lines for skills, language and about-me blocks to parsing to index.html

--- src/js/script.js
+++ src/js/script.js
@@ -26,10 +26,12 @@ titles.forEach((header) => {
 
 async function getData() {
     try {
-        const response = await fetch('src/data/data.json');
-        if (!response.ok)
+        const response = await fetch('http://localhost:8080/dist/data/data.json', {cache: "no-store"});
+        if (!response.ok){
             throw new Error('Помилка при завантаженні даних');
+        }
         const data = await response.json();
+        console.log(data);
         renderData(data);
     } catch (error) {
         console.error('Помилка під час отримання даних:', error);
@@ -37,11 +39,49 @@ async function getData() {
 }
 
 function renderData(data) {
-    const container = document.getElementById('data-container');
-    data.forEach(item => {
-        const div = document.createElement('div');
-        div.textContent = `Year: ${item.year}, Count: ${item.count}`;
-        container.appendChild(div);
+    // About Me Section
+    document.getElementById('about-me-title').textContent = data.aboutMe.title;
+    document.getElementById('about-me-description').textContent = data.aboutMe.description;
+
+    // Personal Info Section
+    document.getElementById('personal-name').textContent = data.personalInfo.name;
+    document.getElementById('personal-job-title').textContent = data.personalInfo.jobTitle;
+
+    document.getElementById('job-name').textContent = data.nameEx;
+    // Job Experience Section
+    const jobExperienceContainer = document.getElementById('job-experience');
+    jobExperienceContainer.innerHTML = ''; // Clear previous content
+    data.jobExperience.forEach(job => {
+        const jobDiv = document.createElement('div');
+        jobDiv.classList.add('job-item');
+        jobDiv.innerHTML = `
+            <h3>${job.title}</h3>
+            <span>${job.years}</span>
+            <p><i>${job.company}</i></p>
+            <p>${job.description}</p>
+        `;
+        jobExperienceContainer.appendChild(jobDiv);
     });
+
+    document.getElementById('skills-name').textContent = data.nameSk;
+    // Skills Section
+    const skillsContainer = document.getElementById('skills');
+    skillsContainer.innerHTML = ''; // Clear previous content
+    data.skills.forEach(skill => {
+        const skillDiv = document.createElement('div');
+        skillDiv.classList.add('skill-item');
+        skillDiv.innerHTML = `
+            <h6>${skill.name}</h6>
+            <progress max="100" value="${skill.level}"></progress>
+        `;
+        skillsContainer.appendChild(skillDiv);
+    });
+
+
+    document.getElementById('language-name').textContent = data.nameLan;
+    document.getElementById('hobby-name').textContent = data.nameHob;
+
 }
+
 getData();
+setInterval(getData, 5000);
